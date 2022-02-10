ab61a16ab6a56a83eb98065345d2d446e37bdcdd
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2022-02-10 16:24:48 -0500

    Add files via upload

---
+++ Info/AlmacenInfo.cs
@@ -0,0 +1,35 @@
+﻿using Modelo;
+using Modelo.Empresa;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Info
+{
+    public class AlmacenInfo
+    {
+
+
+        public static void mostrarAlmacen(Almacen almacen)
+        {
+            string mensaje =  String.Format(
+                "ID: {0} \t Nombre: {1}\t Ubicacion: {2}\n",
+                almacen.AlmacenId,
+                almacen.Nombre,
+                almacen.Ubicacion
+            );
+            
+            Console.WriteLine(mensaje);
+        }
+
+        public static void mostrarAlmacenes(List<Almacen> listaAlmacen)
+        {
+            Console.WriteLine("\t\t -Almacenes-");
+            foreach (var almacen in listaAlmacen)
+            {
+                mostrarAlmacen(almacen);
+            }
+            
+        }
+    }
+}
---
+++ Info/MovimientoInfo.cs
@@ -0,0 +1,40 @@
+﻿using Modelo;
+using Modelo.Empresa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info
+{
+    public class MovimientoInfo
+    {
+        public static void mostrarMovimiento(Movimiento movimiento)
+        {
+            string mensaje = String.Format(
+                "ID: {0}\t Tipo de Movimiento: {1}\t Estado: {2} Fecha de inicio: {3}\t Fecha de fin: {4} ",
+                movimiento.MovimientoId,
+                movimiento.TipoMovimiento,
+                movimiento.Estado,
+                movimiento.FechaInicio,
+                movimiento.FechaFin.HasValue ? movimiento.FechaFin : "------"
+
+            );
+            Console.WriteLine(mensaje);
+            PedidoInfo.mostrarPedidos(movimiento.Pedidos);
+            Console.WriteLine("");
+        }
+
+        public static void mostrarMovimientos(List<Movimiento> lista)
+        {
+            Console.WriteLine("\t\t -Movimientos-");
+            foreach (Movimiento movimiento in lista)
+            {
+                mostrarMovimiento(movimiento);
+            }
+
+
+        }
+    }
+}
